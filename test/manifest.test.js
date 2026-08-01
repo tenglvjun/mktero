d696a31e47621b339cc8008092401f88c8eb1b45
Tony Teng <tenglvjun@gmail.com>
2026-08-01 09:57:12 +0800

    release: prepare v0.2.2

--- test/manifest.test.js
+++ test/manifest.test.js
@@ -36,7 +36,7 @@ test('declares the scalable Mktero logo for extension surfaces', () => {
 });
 
 test('keeps the installable package version metadata consistent', () => {
-    assert.equal(manifest.version, '0.2.1');
+    assert.equal(manifest.version, '0.2.2');
     assert.equal(packageMetadata.version, manifest.version);
     assert.equal(packageLock.version, manifest.version);
     assert.equal(packageLock.packages[''].version, manifest.version);
